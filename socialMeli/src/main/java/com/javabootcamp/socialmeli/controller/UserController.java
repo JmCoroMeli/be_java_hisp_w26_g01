2b0114a4094bf48cbb62c73029f69792c3a64d0a
icugura_meli <ignacio.cugura@mercadolibre.com>
2024-04-24 16:53:59 -0300

    fix @PostMapping anotation

--- socialMeli/src/main/java/com/javabootcamp/socialmeli/controller/UserController.java
+++ socialMeli/src/main/java/com/javabootcamp/socialmeli/controller/UserController.java
@@ -3,11 +3,11 @@ package com.javabootcamp.socialmeli.controller;
 import com.javabootcamp.socialmeli.dto.SellerWithFollowersDTO;
 import com.javabootcamp.socialmeli.dto.ResponseDto;
 import com.javabootcamp.socialmeli.service.IUserService;
-import com.javabootcamp.socialmeli.repository.UserRepository;
-import com.javabootcamp.socialmeli.service.IUserService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
