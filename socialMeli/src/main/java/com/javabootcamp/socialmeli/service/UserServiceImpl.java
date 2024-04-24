414631b76d8954a7535b9266d910c30345740de3
frmoises <franco.fmoises@mercadolibre.com>
2024-04-24 17:09:28 -0300

    add follow validation for clients and sellers

--- socialMeli/src/main/java/com/javabootcamp/socialmeli/service/UserServiceImpl.java
+++ socialMeli/src/main/java/com/javabootcamp/socialmeli/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.javabootcamp.socialmeli.service;
 import com.javabootcamp.socialmeli.dto.ClientDto;
 import com.javabootcamp.socialmeli.dto.ResponseDto;
 import com.javabootcamp.socialmeli.dto.SellerDto;
+import com.javabootcamp.socialmeli.enums.UserType;
 import com.javabootcamp.socialmeli.exception.EntityNotFoundException;
+import com.javabootcamp.socialmeli.exception.IllegalActionException;
 import com.javabootcamp.socialmeli.model.User;
 import com.javabootcamp.socialmeli.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -34,8 +36,14 @@ public class UserServiceImpl implements IUserService{
     public ResponseDto addFollower(Integer followerdId, Integer followedId) {
         User follower = userRepository.findById(followerdId)
                 .orElseThrow(() -> new EntityNotFoundException("Follower not found"));
+        if (follower.getUserType() == UserType.SELLER) {
+            throw new IllegalActionException("Seller cannot follow");
+        }
         User followed = userRepository.findById(followedId)
                 .orElseThrow(() -> new EntityNotFoundException("Followed not found"));
+        if (followed.getUserType() == UserType.CLIENT) {
+            throw new IllegalActionException("Cannot follow a client");
+        }
         followService.addFollow(follower, followed);
         return new ResponseDto("Follower added succesfully");
     }
